9536ad57a801792ec89357adde8153436a272a7c
rafdan28 <rafdanna28@gmail.com>
2023-06-23 10:55:08 +0200

    fix: aggiunta delete cascade su reservationlist di utente

--- src/main/java/it/osmci/polisportiva/model/User.java
+++ src/main/java/it/osmci/polisportiva/model/User.java
@@ -64,6 +64,7 @@ public class User {
 
     @JsonIgnore
     @OneToMany(mappedBy = "user", cascade = CascadeType.REMOVE)
+    @OnDelete(action = OnDeleteAction.CASCADE)
     private List<Reservation> reservationList = new LinkedList<>();
 
     public Long getId() {
