5b70c781af98f8dec1d543ee9131614c161b4b51
rafdan28 <rafdanna28@gmail.com>
2023-07-06 23:10:05 +0200

    fix: aggiunge scrittura a video di docker stats

--- docker-stats.ps1
+++ docker-stats.ps1
@@ -2,5 +2,6 @@ while($true) {
     $output = docker stats --no-stream
     $output | Out-File -Append -Encoding utf8 stats.csv
     "`r`n" | Out-File -Append -Encoding utf8 stats.csv
+    Write-Host $output
     Start-Sleep -Seconds 1
 }
